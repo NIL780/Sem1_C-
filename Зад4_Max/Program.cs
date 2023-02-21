d230a78ee2bead2ba45ebcc515239ce9968f6f9b
NATA <armida780@mail.ru>
2023-02-21 16:40:47 +0300

    Max Number result

---
+++ Зад4_Max/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число A: ");
+int NumberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int NumberB = int.Parse(Console.ReadLine());
+Console.Write("Введите число C: ");
+int NumberC = int.Parse(Console.ReadLine());
+int max = Convert.ToInt32(Math.Max (NumberC, Math.Max(NumberA,NumberB)));
+
+Console.WriteLine($"Максимальное число из трех значений: ({max})");
+
