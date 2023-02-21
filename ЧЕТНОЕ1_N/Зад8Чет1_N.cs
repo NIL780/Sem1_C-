e51acf9d4f9c5d4ecb4d1ce40574a6ab0bd9660c
NATA <armida780@mail.ru>
2023-02-21 19:15:03 +0300

    четное 1_N

---
+++ ЧЕТНОЕ1_N/Зад8Чет1_N.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+int sqr=0;
+
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32 (int.Parse(Console.ReadLine()));
+while (sqr<=Number)
+    {
+        if(sqr%2==0)
+        {Console.WriteLine($"Четные числа: ({sqr})");
+    }
+    sqr++;
+   }
