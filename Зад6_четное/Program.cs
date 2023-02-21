235723bb1406bcd885d8980a2399540170c645b6
NATA <armida780@mail.ru>
2023-02-21 17:10:30 +0300

    етное число

--- Зад6_четное/Program.cs
+++ Зад6_четное/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int Number = int.Parse(Console.ReadLine());
+if(Number%2==0)
+    {Console.WriteLine($"Да,{Number} число является четным");}
+   else
+     {Console.WriteLine($"НЕТ,{Number} число НЕЧЕТНОЕ");}
+     
