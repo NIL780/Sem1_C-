ff498cc0b56222a9a7944cacc0af03c456e0d3f8
NATA <armida780@mail.ru>
2023-02-21 19:53:07 +0300

    последняя цифра

---
+++ Зад7посл_цифра/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("Введите число трехзначное число: ");
+int Number = int.Parse(Console.ReadLine());
+int a = Number % 100;
+int a1 =Number % 10;
+Console.WriteLine($"Последнее число трезначного значения ({a1})");
